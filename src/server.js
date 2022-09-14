ffcdb950a1f1cbc1a297cc94c90d493895b9a16c
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-14 19:47:06 -0300

    fix: added mongoClient

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 dotenv.config();
 const server = express();
+
+server.get('/', (req, res) => {
+  res.sendStatus(200);
+});
 
 server.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
