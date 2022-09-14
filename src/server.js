89bef85639c4130b809f017b06fd1226f73181b8
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-14 20:30:18 -0300

    Test sem env

--- src/server.js
+++ src/server.js
@@ -16,6 +16,11 @@ server.get('/test', (req, res) => {
   res.sendStatus(201);
 });
 
+server.get('/env', (req, res) => {
+  console.log('blank env');
+  res.sendStatus(202);
+});
+
 server.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
