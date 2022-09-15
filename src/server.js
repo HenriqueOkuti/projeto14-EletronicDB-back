4ae6082eaca80501cb86822901dd72f22db0ea88
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-14 21:33:47 -0300

    Fix: Removed test pages

--- src/server.js
+++ src/server.js
@@ -7,26 +7,7 @@ dotenv.config();
 const server = express();
 
 server.get('/', (req, res) => {
-  console.log('Test');
   res.sendStatus(200);
-});
-
-server.get('/test', (req, res) => {
-  console.log('Test get');
-  res.sendStatus(201);
-});
-
-server.get('/env', (req, res) => {
-  console.log('blank env');
-  res.sendStatus(202);
-});
-
-server.get('/test2', (req, res) => {
-  res.sendStatus(203);
-});
-
-server.get('/test3', (req, res) => {
-  res.sendStatus(204);
 });
 
 server.listen(process.env.PORT, () =>
