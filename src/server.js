eca4dfb89d0fcb47b91278862a218a6eb22a3ee6
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-16 19:05:43 -0300

    Product data

--- src/server.js
+++ src/server.js
@@ -2,25 +2,32 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import sendDataToDB from './functions/sendDataToDB.js';
+import db from './db/db.js';
+import { COLLECTIONS } from './enums/collections.js';
 
 dotenv.config();
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-sendDataToDB();
+//sendDataToDB();
 
 server.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
+server.get('/mongo', async (req, res) => {
+  const data = await db.collection(COLLECTIONS.PRODUCTS).find({ name: 'name' });
+  console.log(data);
+  res.send(data);
+});
+
 server.get('/test', (req, res) => {
-  console.log('Test get');
   res.sendStatus(201);
 });
 
-server.get('/test2', (req, res)=>{
-  console.log("Test2 get");
+server.get('/test2', (req, res) => {
+  console.log('Test2 get');
   res.sendStatus(201);
 });
 
