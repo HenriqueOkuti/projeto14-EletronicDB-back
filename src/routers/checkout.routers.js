771200a5c9fd54beaa6b24f49a882b58ac7eb781
HenriqueOkuti <henrique.krastins@gmail.com>
2022-09-19 02:50:02 -0300

    minor fixes

--- src/routers/checkout.routers.js
+++ src/routers/checkout.routers.js
@@ -1,19 +1,10 @@
 import express from 'express';
 import { getOrder, postOrder } from '../controllers/checkout.controllers.js';
-import {
-  verifyCart,
-  verifyCredentials,
-} from '../middlewares/cart.middleware.js';
+import { verifyCredentials } from '../middlewares/cart.middleware.js';
 import { verifyOrders } from '../middlewares/checkout.middleware.js';
 
 const router = express.Router();
-router.get('/checkout', verifyCredentials, verifyCart, verifyOrders, getOrder);
-router.post(
-  '/checkout',
-  verifyCredentials,
-  verifyCart,
-  verifyOrders,
-  postOrder
-);
+router.get('/checkout', verifyCredentials, verifyOrders, getOrder);
+router.post('/checkout', verifyCredentials, verifyOrders, postOrder);
 
 export default router;
